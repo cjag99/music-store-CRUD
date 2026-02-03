f2cb47f56c483df86cefb55689555e5e100ddc64
cjag99 <cjag99@gmail.com>
2026-02-03 11:30:23 +0100

    Stylize welcome hero section and all auth sections

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,9 +21,9 @@
             <x-input-label for="password" :value="__('Password')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+                type="password"
+                name="password"
+                required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -33,16 +33,23 @@
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+                type="password"
+                name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+            <a
+                class="underline text-sm text-[#3B2F2F]
+           hover:text-[#C49A3A]
+           rounded-md focus:outline-none focus:ring-2
+           focus:ring-[#C49A3A] focus:ring-offset-2
+           transition ease-in-out duration-150"
+                href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
+
 
             <x-primary-button class="ms-4">
                 {{ __('Register') }}
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,14 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{
+    $attributes->merge([
+        'class' => '
+            border-[#3B2F2F33]
+            bg-[#FFF8E7]
+            text-[#3B2F2F]
+            focus:border-[#C49A3A]
+            focus:ring-[#C49A3A]
+            rounded-md shadow-sm
+        '
+    ])
+}}>
